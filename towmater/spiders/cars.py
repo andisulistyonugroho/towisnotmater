d21b901431feacadde8190719a7f1f379d42c1d4
andisulistyonugroho <andisulistyonugroho@gmail.com>
2017-07-30 06:34:18 +0700

    add piece of code comment

--- towmater/spiders/cars.py
+++ towmater/spiders/cars.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 import scrapy
 
 class CarsSpider(scrapy.Spider):
@@ -7,18 +8,19 @@ class CarsSpider(scrapy.Spider):
     start_urls = ['http://csndealers.carsalesnetwork.com.au/listing?dealerId=AGC-SELLER-28894%2BAGC-SELLER-14679%2BAGC-SELLER-51021']
             
     def start_requests(self):
+            #-----------------------------------------------------------
+            # set user agent, bot goes undercoverimitate browser 
+            # remove this then the scraper will be rejected by the target
             headers= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
             for url in self.start_urls:
                 yield scrapy.Request(url, headers=headers)
         
     def parse(self, response):
+        #--- wrapper search result
         cars = response.xpath('//div[@class="search-results"]')
         for car in cars:
+            #---- wrapper car list
             x = car.xpath('//div[@class="result-item"]')
             for y in x:
                 name = y.xpath('//h4[@class="result-item-title"]/a/text()').extract_first()
                 yield {'x':name}
-#             # names = car.xpath('//form[@class="result-item-title"]/@method').extract_first()
-# #             for name in names:
-# #                 yield {'name': name}
-#pass

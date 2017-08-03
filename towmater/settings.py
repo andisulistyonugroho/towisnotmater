60c13a6523769aa01bd1b61d322c3361ccc83802
andisulistyonugroho <andisulistyonugroho@gmail.com>
2017-08-03 21:28:44 +0700

    use default header no override

--- towmater/settings.py
+++ towmater/settings.py
@@ -27,22 +27,23 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+   'Accept-Language': 'en',
+   'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+}
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
@@ -88,3 +89,5 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# LOG_LEVEL = 'CRITICAL'
--- towmater/spiders/cars.py
+++ towmater/spiders/cars.py
@@ -6,21 +6,23 @@ class CarsSpider(scrapy.Spider):
     name = 'cars'
     allowed_domains = ['carsalesnetwork.com.au']
     start_urls = ['http://csndealers.carsalesnetwork.com.au/listing?dealerId=AGC-SELLER-28894%2BAGC-SELLER-14679%2BAGC-SELLER-51021']
-            
-    def start_requests(self):
-            #-----------------------------------------------------------
-            # set user agent, bot goes undercoverimitate browser 
-            # remove this then the scraper will be rejected by the target
-            headers= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
-            for url in self.start_urls:
-                yield scrapy.Request(url, headers=headers)
         
     def parse(self, response):
         #--- wrapper search result
         cars = response.xpath('//div[@class="search-results"]')
         for car in cars:
             #---- wrapper car list
-            x = car.xpath('//div[@class="result-item"]')
+            x = car.xpath('.//div[@class="result-item"]')
             for y in x:
-                name = y.xpath('//h4[@class="result-item-title"]/a/text()').extract_first()
-                yield {'x':name}
+                name = y.xpath('.//h4[@class="result-item-title"]/a/text()').extract_first()
+                price = y.xpath('.//div[@class="price"]/a/text()').extract_first()
+                yield {
+                    'name': name,
+                    'price': price
+                }
+        
+        next_page = response.xpath('//ul[@class="pagination pull-right"]/li[@class="active"]/following-sibling::li/a/@href').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+            

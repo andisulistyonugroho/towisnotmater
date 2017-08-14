9079ce7be6db165b211116cab319a4ba0c2d7d17
andisulistyonugroho <andisulistyonugroho@gmail.com>
2017-08-14 23:51:55 +0700

    set meta car_id on detail

--- towmater/spiders/cars.py
+++ towmater/spiders/cars.py
@@ -45,7 +45,7 @@ class CarsSpider(scrapy.Spider):
         for car_detail in car_details:
             the_key = car_detail.xpath('.//th/text()').extract_first()
             the_value = car_detail.xpath('.//td').extract_first()
-            #item['car_id'] = response.meta['car_id']
+            item['car_id'] = response.meta['car_id']
             the_key = the_key.lower().replace(' ','_')
             if the_key in item.fields:
                 item[the_key] = the_value
